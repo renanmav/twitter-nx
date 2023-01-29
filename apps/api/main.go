8522808be6cb0c9337b935c4338c985a1f1176cd
Renan Machado <renan.mav@hotmail.com>
2023-01-29 15:19:01 -0300

    Get started with graphql-go/graphql

--- apps/api/main.go
+++ apps/api/main.go
@@ -1,12 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
 
-func Hello(name string) string {
-	result := "Hello " + name
-	return result
-}
+	"github.com/graphql-go/graphql"
+)
 
 func main() {
-	fmt.Println(Hello("api"))
+	fields := graphql.Fields{
+		"hello": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "world", nil
+			},
+		},
+	}
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("Failed to create new schema, error: %v", err)
+	}
+
+	// Query
+	query := `
+		{
+			hello
+		}
+	`
+	params := graphql.Params{Schema: schema, RequestString: query}
+	r := graphql.Do(params)
+	if len(r.Errors) > 0 {
+		log.Fatalf("Failed to execute graphql operation, errors: %+v", r.Errors)
+	}
+	rJSON, _ := json.Marshal(r)
+	fmt.Printf("%s \n", rJSON) // {"data":{"hello":"world"}}
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module twitter-nx
+
+go 1.19
+
+require github.com/graphql-go/graphql v0.8.0 // indirect
